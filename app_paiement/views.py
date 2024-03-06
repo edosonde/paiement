3af5959de04d0692dd4bfeba6e9fb3cc7947529f
edosonde <christianedosonde07@gmail.com>
2024-03-07 00:22:43 +0100

    CHRIST

--- app_paiement/views.py
+++ app_paiement/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from app_paiement.models.frais import *
 
 # create you views here.
 
@@ -6,7 +7,23 @@ def index(request):
     return render(request,'index.html')
 
 def ffrais(request):
-    return render(request,'frais.html')
+    frais = Frais.objects.all()
+   # frais = Frais.objects.all().count()
+    
+
+    ctx = {"frais": frais,
+           "nbrf": len(frais)
+    }
+
+    return render(request,'frais.html',ctx)
     
 def dash(request):
     return render(request,'dashboard.html')
+
+def deleteFrais(request,id):
+    if request.methode == 'POST':
+        fr = Frais.objects.get(pk=id)
+        Fr.delete()
+        return HttpResponseRedirect('/frais/')
+    else:
+        return HttpResponseRedirect('/frais/')
