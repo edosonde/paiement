ba97ae653c927c69f3f2cf7a7e645ba6c19a7aa0
edosonde <christianedosonde07@gmail.com>
2024-03-08 01:13:50 +0100

    modification

--- app_paiement/admin.py
+++ app_paiement/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+from app_paiement.models.paiement import *
+from app_paiement.models.frais import *
+from app_paiement.models.eleve import *
+from app_paiement.models.classe import *
 
 # Register your models here.
 
+admin.site.register(paiement)
+admin.site.register( Frais)
+admin.site.register(Eleve)
+admin.site.register(Classe)
